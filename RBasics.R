23570b57a9d17001d2d07baf46e8c6954f6038cf
suraj062000 <153431428+suraj062000@users.noreply.github.com>
2023-12-10 17:25:20 +0530

    Add files via upload

---
+++ RBasics.R
@@ -0,0 +1,29 @@
+s = 'This is my 1st variable'
+s
+rm(s)
+
+b <- 20
+b
+
+
+b <- T
+class(b)
+
+city <- 'PUNE'
+class(city)
+
+f=10.2
+class(f)
+
+
+p = '411019'
+class(p)
+
+I = 20L
+class(I)
+
+i=2
+class(i)
+
+bool = FALSE
+class(bool)
